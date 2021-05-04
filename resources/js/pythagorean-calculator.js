8021a1e83a55dcb22e100244765847d0bae74f70
gspetillo <gspetillo@gmail.com>
2021-05-04 20:53:05 -0300

    add copy button, footer and icons

--- resources/js/pythagorean-calculator.js
+++ resources/js/pythagorean-calculator.js
@@ -2,8 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         formFields: {
-            labels:['Cateto Oposto', 'Cateto Adjacente'], 
-            descriptions: ['Digite o valor do cateto oposto','Digite o valor do cateto adjacente']
+            labels: ['Cateto Oposto', 'Cateto Adjacente'],
+            descriptions: ['Digite o valor do cateto oposto', 'Digite o valor do cateto adjacente']
         },
         picked: 'Hipotenusa',
         field1: null,
@@ -11,13 +11,13 @@ var app = new Vue({
         result: '-',
     },
     methods: {
-        calculationType() {
+        setLabels() {
             if (this.picked == 'Hipotenusa') {
                 this.formFields.labels = ['Cateto Oposto', 'Cateto Adjacente']
-                this.formFields.descriptions = ['Digite o valor do cateto oposto','Digite o valor do cateto adjacente']
+                this.formFields.descriptions = ['Digite o valor do cateto oposto', 'Digite o valor do cateto adjacente']
             } else {
                 this.formFields.labels = ['Cateto', 'Hipotenusa']
-                this.formFields.descriptions = ['Digite o valor do cateto','Digite o valor da hipotenusa']
+                this.formFields.descriptions = ['Digite o valor do cateto', 'Digite o valor da hipotenusa']
             }
             this.calculate()
         },
@@ -25,25 +25,45 @@ var app = new Vue({
         calculate() {
             if (this.field1 && this.field2) {
                 if (this.picked == 'Hipotenusa') {
-                    this.result = this.calcultateHypotenuse()
+                    this.result = this.calculateHypotenuse()
                 } else {
                     this.result = this.calculateSide()
                 }
             }
         },
 
-        calcultateHypotenuse() {
+        calculateHypotenuse() {
             return Math.sqrt(Math.pow(this.field1, 2) + Math.pow(this.field2, 2))
         },
 
         calculateSide() {
-            return Math.sqrt(Math.pow(this.field2, 2) - Math.pow(this.field1, 2))
+            if(this.field2>=this.field1){
+                return Math.sqrt(Math.pow(this.field2, 2) - Math.pow(this.field1, 2))
+            }else{ 
+                return 'O valor da hipotenusa não pode ser inferior ao do cateto'
+            }
+            
         },
 
         clearFields() {
             this.field1 = this.field2 = null
             this.result = '-'
-        }
+        },
+
+        copyResultToClipboard() {
+            let r = document.createRange()
+            r.selectNode(document.getElementById('result'))
+            window.getSelection().removeAllRanges()
+            window.getSelection().addRange(r)
+            document.execCommand('copy')
+            window.getSelection().removeAllRanges()
+            swal({
+                title: `📄 Resultado "${this.result}" copiado.`,
+                text: `O valor está na Área de Transferência.`,
+                timer: 2000,
+                showConfirmButton: false
+            });
+        },
 
     }
 })
